74b6d422bdd20b028ef2bbfcd08774a0006a60a3
Saku <91819637+Saku2525@users.noreply.github.com>
2024-01-07 18:37:54 +0900

    Update 3-5-2.c

--- Chap03/3-5-2.c
+++ Chap03/3-5-2.c
@@ -1,19 +1,19 @@
-/****************************************************/
-/*	じゃんけんゲーム								*/
+/********************************************************/
+/*	じゃんけんゲーム				*/
 /*	コンピュータが5回ごとに後出し(勝負回数制限なし)	*/
-/****************************************************/
+/********************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-/********************/
+/************************/
 /*	スコア用構造体	*/
-/********************/
+/************************/
 typedef struct {
 	int draw;	/*	引き分けた回数	*/
-	int lose;	/*	勝った回数		*/
-	int win;	/*	負けた回数		*/
+	int lose;	/*	勝った回数	*/
+	int win;	/*	負けた回数	*/
 } Score;
 
 char *mHd[] = { "グー", "チョキ", "パー" };
@@ -48,7 +48,7 @@ void jyanken(int num, int *human, int *comp)
 	} while (*human < 0 || *human > 2);
 
 	if (num % 5 == 0) {
-		*comp = (*human + 2) % 3;			/*	後出し			*/
+		*comp = (*human + 2) % 3;		/*	後出し			*/
 	}
 }
 
@@ -70,7 +70,7 @@ void update_score(int judge, Score *score)
 void disp_judge(int judge)
 {
 	switch (judge) {
-		case 0:	printf("引き分けです。\n");		break;
+		case 0:	printf("引き分けです。\n");	break;
 		case 1:	printf("あなたの負けです。\n");	break;
 		case 2:	printf("あなたの勝ちです。\n");	break;
 	}
@@ -102,11 +102,11 @@ int main(void)
 	do {
 		jyanken(num++, &human, &comp);		/*	じゃんけん実行		*/
 		printf("私は%sで、あなたは%sです。\n", mHd[comp], mHd[human]);
-											/*	手を表示			*/
-		judge = ((human - comp) + 3) % 3;	/*	勝敗を判定			*/
+							/*	手を表示		*/
+		judge = ((human - comp) + 3) % 3;	/*	勝敗を判定		*/
 		update_score(judge, &score);		/*	スコアを更新		*/
-		disp_judge(judge);					/*	判定結果を表示		*/
-		retry = confirm_retry();			/*	再挑戦するか確認	*/
+		disp_judge(judge);			/*	判定結果を表示		*/
+		retry = confirm_retry();		/*	再挑戦するか確認	*/
 	} while (retry == 1);
 
 	printf("%d勝%d負%d分けでした。\n", score.win, score.lose, score.draw);
