02c02cadc5682c310a5f502e351b884a0caa0c84
Saku <91819637+Saku2525@users.noreply.github.com>
2024-01-10 08:43:36 +0900

    Update 3-8-2.c

--- Chap03/3-8-2.c
+++ Chap03/3-8-2.c
@@ -1,19 +1,19 @@
-/********************************************/
-/*	じゃんけんゲーム						*/
+/************************************************/
+/*	じゃんけんゲーム			*/
 /*	4人(コンピュータ3人) (勝負回数制限なし)	*/
-/********************************************/
+/************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-/********************/
-/*	スコア用構造体	*/
-/********************/
+/*********************/
+/*---スコア用構造体---*/
+/*********************/
 typedef struct {
 	int draw;	/*	引き分けた回数	*/
-	int lose;	/*	勝った回数		*/
-	int win;	/*	負けた回数		*/
+	int lose;	/*	勝った回数	*/
+	int win;	/*	負けた回数	*/
 } Score;
 
 char *mHd[] = { "グー", "チョキ", "パー" };
@@ -90,7 +90,7 @@ void update_score(int judge, Score *score)
 {
 	switch (judge) {
 		case 0:	score->draw++;	break;
-		case 1:	score->lose++;;	break;
+		case 1:	score->lose++;	break;
 		case 2:	score->win++;	break;
 	}
 }
@@ -101,7 +101,7 @@ void update_score(int judge, Score *score)
 void disp_judge(int judge)
 {
 	switch (judge) {
-		case 0:	printf("引き分けです。\n");		break;
+		case 0:	printf("引き分けです。\n");	break;
 		case 1:	printf("あなたの負けです。\n");	break;
 		case 2:	printf("あなたの勝ちです。\n");	break;
 	}
@@ -131,14 +131,14 @@ int main(void)
 	initialize(&score);
 
 	do {
-		jyanken(&human, &comp1, &comp2, &comp3);			/*	じゃんけん実行		*/
+		jyanken(&human, &comp1, &comp2, &comp3);		/*	じゃんけん実行		*/
 		printf("コンピュータ1は%sで、コンピュータ2は%sで、コンピュータ3は%sで、あなたは%sです。\n"
 											, mHd[comp1], mHd[comp2], mHd[comp3], mHd[human]);
-															/*	手を表示			*/
-		judge = judge_winlose(human, comp1, comp2, comp3);	/*	勝敗を判定			*/
-		update_score(judge, &score);						/*	スコアを更新		*/
-		disp_judge(judge);									/*	判定結果を表示		*/
-		retry = confirm_retry();							/*	再挑戦するか確認	*/
+									/*	手を表示		*/
+		judge = judge_winlose(human, comp1, comp2, comp3);	/*	勝敗を判定		*/
+		update_score(judge, &score);				/*	スコアを更新		*/
+		disp_judge(judge);					/*	判定結果を表示		*/
+		retry = confirm_retry();				/*	再挑戦するか確認	*/
 	} while (retry == 1);
 
 	printf("%d勝%d負%d分けでした。\n", score.win, score.lose, score.draw);
